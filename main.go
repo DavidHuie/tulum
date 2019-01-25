1d99ac24a242c9eb2f33271d064f2f209ed54541
David Huie <dahuie@gmail.com>
2019-01-24 19:48:04 -0800

    Set key default differently

--- main.go
+++ main.go
@@ -17,26 +17,21 @@ func main() {
 
 	flag.BoolVar(&enc, "enc", true, "encrypt from stdin, writing output to stdout")
 	flag.BoolVar(&dec, "dec", false, "decrypt from stdin, writing output to stdout")
-	flag.StringVar(&keyFile, "key", "", "the path to the key")
+	flag.StringVar(&keyFile, "key", "key.asc", "the path to the key")
 	flag.Parse()
 
-	// Only one of enc or dec should be true.
+	// Only one of enc or dec should be true, but enc is always
+	// true by default.
 	if dec {
 		enc = false
 	}
 
 	switch {
 	case enc:
-		if keyFile == "" {
-			keyFile = "key.asc"
-		}
 		if err := encrypt(os.Stdin, os.Stdout, keyFile); err != nil {
 			log.Fatalf("Error encrypting: %s", err)
 		}
 	case dec:
-		if keyFile == "" {
-			keyFile = "key.asc"
-		}
 		if err := decrypt(os.Stdin, os.Stdout, keyFile); err != nil {
 			log.Fatalf("Error decrypting: %s", err)
 		}
